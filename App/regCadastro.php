a8d9d674ce4ccb579807afea8853298c84933cf8
Jadilson Guedes <jadilson12@gmail.com>
2018-09-24 23:45:37 -0300

    App: alterou o modelo de data para 00-12-2018 22:10:23

--- App/regCadastro.php
+++ App/regCadastro.php
@@ -10,7 +10,7 @@ require_once "Candidato.php";
 	$fone = filter_input(INPUT_POST,"fone",FILTER_SANITIZE_MAGIC_QUOTES);
 	$cidade = filter_input(INPUT_POST,"cidade",FILTER_SANITIZE_MAGIC_QUOTES);
 	$serie  = filter_input(INPUT_POST,"serie",FILTER_SANITIZE_MAGIC_QUOTES);
-		
+
 		if(!empty($nome) && !empty($mail) && !empty($fone) && !empty($cidade) && !empty($serie)){
 		$cad = new Candidato;
 			$cad->setNome($nome);
@@ -26,32 +26,32 @@ require_once "Candidato.php";
 					$arr['serie'] = $cad->getSerie();
 					$arr['cand_cel'] = $cad->getFone();
 					$arr['cand_cidade'] = $cad->getCidade();
-					$arr['date'] = date('Y - d - m H:i:s');
+					$arr['date'] = date('d-m-Y H:i:s');
 					$exe = $cad->create("candidato",$arr);
 						if($exe):
 							echo "<p class='bg-success'>Cadastrado com sucesso!</p>";
 							$_SESSION['mail'] = $cad->getMail();
 							//$_SESSION['logado'] = true;
 							$_SESSION['candidato'] = $arr;
-							
+
 							header("refresh:0.1;url=questao.php");
 							echo "Olá $nome";
 						else:
 							echo '<p class="bg-danger">Erro ao realizar cadastro.</p>';
 							$_SESSION['logado'] = false;
-							
+
 						endif;
 		}
 				/*}else{
 					echo "<p class='bg-danger' >E-mail invalido!</p>";
 				}*/
 	//echo $_SESSION['mensagem'];
-//}				
-if(isset($_POST['send'])){
+//}
+//if(isset($_POST['send'])){
 	$_SESSION['valor'] += isset($_POST['op']) ? $_POST['op'] : "" ;
 }
 if(!empty($_SESSION['candidato'])){
-    
+
     return true;
 }else{
     return false;
